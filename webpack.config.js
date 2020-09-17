91bacc1dd3ffb428f227cf4b550a38e836183b8d
ldou <1914017555@qq.com>
2020-09-18 00:15:49 +0800

    webpack初始化

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './path/to/my/entry/file.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
-    }
+  entry: "./src/app.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react"], //babel-preset-env转码插件
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", //模板
+    }),
+  ],
 };
