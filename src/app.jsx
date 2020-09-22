c728acd385c8a6a8c38da28e314847a5b2ab209b
ldou <1914017555@qq.com>
2020-09-22 23:04:40 +0800

    state,props一些练习

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Component extends React.Component {
+class Father extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "rose",
-      age: 18,
+      age: 15,
+      name: "lisa",
+      bgColor: "#ddd",
     };
-    //this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
-
   handleClick() {
     this.setState({
       age: this.state.age + 1,
     });
   }
-
+  onChangeBgColor(color) {
+    this.setState({
+      bgColor: color,
+    });
+  }
   render() {
     return (
-      <div>
-        <h1>I am {this.state.name}</h1>
-        <p>I am {this.state.age} years old</p>
-        {/* <button onClick={this.handleClick}>加一岁</button> */}
-        <button
-          onClick={(e) => {
-            this.handleClick(e);
+      <div style={{ background: this.state.bgColor }}>
+        <h1>name:{this.state.name}</h1>
+        <p>age:{this.state.age}</p>
+        <button onClick={this.handleClick}>加一岁</button>
+        {/* 在子组件写回调函数 */}
+        <Child
+          changeColor={(color) => {
+            this.onChangeBgColor(color);
           }}
-        >
-          加一岁
-        </button>
+        />
       </div>
     );
   }
 }
 
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.changeColor("red"); //调用函数改变父组件背景色
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>改变father的背景色：</button>;
+  }
+}
+
 ReactDOM.render(
   <div>
-    <Component />
+    <Father />
   </div>,
   document.getElementById("app")
 );
--- test/jsx.js
+++ test/jsx.js
@@ -63,3 +63,264 @@ ReactDOM.render(
   </div>,
   document.getElementById("app")
 );
+
+//事件处理
+
+class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "rose",
+      age: 18,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      age: this.state.age + 1,
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      age: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I am {this.state.name}</h1>
+        <p>I am {this.state.age} years old</p>
+        <button onClick={this.handleClick}>加一岁</button>
+        {/* <button onClick={(e) => {this.handleClick(e);}}>
+          加一岁
+        </button> */}
+        <input
+          type="text"
+          onChange={(e) => {
+            this.handleChange(e);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Component />
+  </div>,
+  document.getElementById("app")
+);
+
+//这是组件的组合方式
+class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "rose",
+      age: 18,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      age: this.state.age + 1,
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      age: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I am {this.state.name}</h1>
+        <p>I am {this.state.age} years old</p>
+        <button onClick={this.handleClick}>加一岁</button>
+        {/* <button onClick={(e) => {this.handleClick(e);}}>
+          加一岁
+        </button> */}
+        <input
+          type="text"
+          onChange={(e) => {
+            this.handleChange(e);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+class Title extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(props) {
+    return <h1>{this.props.children}</h1>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="">
+        {/* 容器式组件 */}
+        <Title>
+          <span>app span</span>
+          <a href="">link</a>
+        </Title>
+        <hr />
+        {/* 单纯组件 */}
+        <Component />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("app")
+);
+
+//父子组件改变值
+
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.changeColor("red");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>父组件的背景色： {this.props.bgColor}</h1>
+        <button onClick={this.handleClick}>改变背景色</button>
+        {/* <button onClick={(e) => {this.handleClick(e);}}>
+          加一岁
+        </button> */}
+      </div>
+    );
+  }
+}
+
+class Father extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColor: "#999",
+    };
+  }
+  onBgColorChange(color) {
+    this.setState({
+      bgColor: color,
+    });
+  }
+  render(props) {
+    return (
+      <div style={{ background: this.state.bgColor }}>
+        <Child
+          bgColor={this.state.bgColor}
+          changeColor={(color) => {
+            this.onBgColorChange(color);
+          }}
+        />
+        ;
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Father />
+  </div>,
+  document.getElementById("app")
+);
+
+//兄弟之间改变值
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Child1 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.changeChild2Color("red");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>child1: </h1>
+        <button onClick={this.handleClick}>改变child2背景色</button>
+        {/* <button onClick={(e) => {this.handleClick(e);}}>
+          加一岁
+        </button> */}
+      </div>
+    );
+  }
+}
+
+class Child2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{ background: this.props.child2BgColor }}>
+        <h1>child2背景色: {this.props.changeChild2BgColor}</h1>
+      </div>
+    );
+  }
+}
+
+class Father extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      child2BgColor: "#999",
+    };
+  }
+  changeChild2BgColor(color) {
+    this.setState({
+      child2BgColor: color,
+    });
+  }
+  render(props) {
+    return (
+      <div>
+        <Child1
+          changeChild2Color={(color) => {
+            this.changeChild2BgColor(color);
+          }}
+        />
+        <Child2 child2BgColor={this.state.child2BgColor} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Father />
+  </div>,
+  document.getElementById("app")
+);
