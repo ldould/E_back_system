72174ba08acb773f0b748e0a86cc8419aad6d611
ldou <ldou@dcampus.com>
2020-10-19 10:05:52 +0800

    用户列表制作

--- src/utils/mm.jsx
+++ src/utils/mm.jsx
@@ -1,77 +1,77 @@
 //类可以作隔离的作用域
 
 class MUtil {
-  request(param) {
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        type: param.type || "get",
-        url: param.url || "",
-        dataType: param.dataType || "json",
-        data: param.data || null,
-        success(res) {
-          //数据请求成功
-          if (0 === res.status) {
-            typeof resolve === "function" && resolve(res.data, res.msg);
-          }
-          //没有登录状态，强制登
-          else if (10 === res.status) {
-            this.doLogin();
-          } else {
-            typeof reject === "function" && reject(res.msg || res.data);
-          }
-        },
-        error(err) {
-          typeof reject === "function" && reject(err.statusText);
-        },
-      });
-    });
-  }
-  //跳转登录
-  doLogin() {
-    window.location.href =
-      "/login?redirect=" + encodeURIComponent(window.location.pathname);
-  }
-  //获取url参数
-  getUrlParam(name) {
-    //xxxx.com?param=123&param1=456
-    let queryString = window.location.search.split("?")[1] || ""; //param=123&param1=456
-    // console.log(queryString, "queryString");
-    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
-      result = queryString.match(reg);
-    //result:['param=123','','123','&']
-    // console.log(result, "result");
-    return result ? encodeURIComponent(result[2]) : null;
-  }
-  //错误提示
-  errorTips(errMsg) {
-    alert(errMsg || "错误");
-  }
-  //存储
-  setStorage(name, data) {
-    let dataType = typeof data;
-    if (dataType === "object") {
-      //json对象
-      window.localStorage.setItem(name, JSON.stringify(data));
+    request(param) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: param.type || "get",
+                url: param.url || "",
+                dataType: param.dataType || "json",
+                data: param.data || null,
+                success(res) {
+                    //数据请求成功
+                    if (0 === res.status) {
+                        typeof resolve === "function" && resolve(res.data, res.msg);
+                    }
+                    //没有登录状态，强制登
+                    else if (10 === res.status) {
+                        this.doLogin();
+                    } else {
+                        typeof reject === "function" && reject(res.msg || res.data);
+                    }
+                },
+                error(err) {
+                    typeof reject === "function" && reject(err.statusText);
+                },
+            });
+        });
     }
-    //基础类型不用序列化
-    else if (["number", "string", "boolean"].indexOf(dataType) >= 0) {
-      window.localStorage.setItem(name, data);
-    } else {
-      alert("该类型不能用于本地存储");
+    //跳转登录
+    doLogin() {
+        window.location.href =
+            "/login?redirect=" + encodeURIComponent(window.location.pathname);
     }
-  }
-  //取出存储内容
-  getStorage(name) {
-    let data = window.localStorage.getItem(name);
-    if (data) {
-      return JSON.parse(data);
-    } else {
-      return "";
+    //获取url参数
+    getUrlParam(name) {
+        //xxxx.com?param=123&param1=456
+        let queryString = window.location.search.split("?")[1] || ""; //param=123&param1=456
+        // console.log(queryString, "queryString");
+        let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
+            result = queryString.match(reg);
+        //result:['param=123','','123','&']
+        // console.log(result, "result");
+        return result ? encodeURIComponent(result[2]) : null;
     }
-  }
-  //删除本地存储
-  removeStorage(name) {
-    window.localStorage.removeItem(name);
-  }
+    //错误提示
+    errorTips(errMsg) {
+        alert(errMsg || "错误");
+    }
+    //存储
+    setStorage(name, data) {
+        let dataType = typeof data;
+        if (dataType === "object") {
+            //json对象
+            window.localStorage.setItem(name, JSON.stringify(data));
+        }
+        //基础类型不用序列化
+        else if (["number", "string", "boolean"].indexOf(dataType) >= 0) {
+            window.localStorage.setItem(name, data);
+        } else {
+            alert("该类型不能用于本地存储");
+        }
+    }
+    //取出存储内容
+    getStorage(name) {
+        let data = window.localStorage.getItem(name);
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return "";
+        }
+    }
+    //删除本地存储
+    removeStorage(name) {
+        window.localStorage.removeItem(name);
+    }
 }
 export default MUtil;
